4a455424726424cd8f1a17b6976de49c23bcf2ce
Vladimir Klimov <vladimirklimoff@yandex.ru>
2019-01-19 16:12:15 +0500

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string[] rules = new[] { "*a->a*", "*b->b*", "*->.b", "Л->*" };
+
+            string word = "abbaaa";
+            Console.WriteLine(word);
+            bool ruleApplied = false;
+            bool lastRule = false;
+
+            do
+            {
+                ruleApplied = false;
+
+                foreach (string rule in rules)
+                {
+                    string[] splitters = new[] { "->.", "->" };
+                    string[] ruleParts = rule.Split(splitters, StringSplitOptions.None);
+
+                    string L = ruleParts[0];
+                    string R = ruleParts[1];
+
+                    lastRule = rule.Contains("->.");
+
+                    if (L == "Л")
+                    {
+                        word = R + word;
+                        ruleApplied = true;
+                        Console.WriteLine($"{word} ({rule})");
+                        break;
+                    }
+                    else if (word.Contains(L))
+                    {
+                        Regex search = new Regex(Regex.Escape(L));
+                        word = search.Replace(word, R, 1, 0);
+                        Console.WriteLine($"{word} ({rule})");
+
+                        ruleApplied = true;
+                        break;
+                    }
+                }
+
+            } while (ruleApplied && !lastRule);
+        }
+    }
+}
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+            //string[] rules = new[] { "*a->a*", "*b->b*", "*->.b", "Л->*" };
+            string[] rules = textBoxRules.Lines;
+            string word = textBoxWord.Text;
+
+            Console.WriteLine(word);
+            bool ruleApplied = false;
+            bool lastRule = false;
+
+            do
+            {
+                ruleApplied = false;
+
+                //проверить правильность ввода правил
+                string pattern = @" ";
+                //пошаговое выполнение, подсветка
+
+
+                foreach (string rule in rules)
+                {
+                    string[] splitters = new[] { "->.", "->" };
+                    string[] ruleParts = rule.Split(splitters, StringSplitOptions.None);
+
+                    string L = ruleParts[0];
+                    string R = ruleParts[1];
+
+                    lastRule = rule.Contains("->.");
+
+                    if (L == "Л")
+                    {
+                        word = R + word;
+                        ruleApplied = true;
+                        Console.WriteLine($"{word} ({rule})");
+                        break;
+                    }
+                    else if (word.Contains(L))
+                    {
+                        Regex search = new Regex(Regex.Escape(L));
+                        word = search.Replace(word, R, 1, 0);
+                        Console.WriteLine($"{word} ({rule})");
+
+                        ruleApplied = true;
+                        break;
+                    }
+                }
+
+            } while (ruleApplied && !lastRule);
+
+            textBoxWord.Text = word;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //начальные значения
+            textBoxWord.Text = "abba";
+            textBoxRules.AppendText("*a->a*");
+            textBoxRules.AppendText("\r\n*b->b*");
+            textBoxRules.AppendText("\r\n*->.b");
+            textBoxRules.AppendText("\r\nЛ->*");
+        }
+    }
+}
